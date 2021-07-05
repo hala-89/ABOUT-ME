776f9036289106531d53ff5fb0033dc4362aea86
hala-89 <halamahmoud_89@yahoo.com>
2021-07-05 21:08:43 +0300

    finish all lab

---
+++ js/app.js
@@ -0,0 +1,103 @@
+
+  'use strict' 
+
+let userName=prompt('what\'s your name?');
+
+console.log( 'hello' , userName);
+
+alert('welcom '+ userName + ' let go  play my game' );
+
+
+/////////////////////////////////
+
+
+  let firstone=prompt('is im  amum?');
+
+
+ firstone=firstone.toLowerCase();
+
+if (firstone=="yes" || firstone=="y" ) {
+
+  console.log( firstone+" that true");
+
+     alert( userName + "YOUR answer is yes");
+ } else{
+   
+    console.log(' oh thats wrong');
+   
+      alert( userName + "YOUR answer is false");
+
+ }
+
+//////////////////////////////////
+
+let secandone=prompt('is am  like ice?');
+
+secandone=secandone.toLowerCase();
+
+if (secandone=="yes" || secandone=="y" ) {
+
+  console.log( secandone+"  that true");
+
+     alert( userName + "YOUR answer is yes");
+ } else{
+   
+    console.log(' oh thas wrong');
+   
+      alert( userName + "YOUR answer is false");
+
+ }
+//////////////////////////////////
+
+
+let third=prompt('is am   have car ?');
+
+third=third.toLowerCase();
+
+if (third=="yes" || third=="y" ) {
+
+  console.log( third+"  that true");
+
+     alert( userName + "YOUR answer is yes");
+ } else{
+   
+    console.log( ' oh thas wrong');
+   
+   alert( userName + 'YOUR answer is false') ;}
+
+   ///////////////////////
+
+   let foure=prompt('is am  like read book?');
+
+foure=foure.toLowerCase();
+
+if (foure=="yes" || foure=="y" ) {
+
+  console.log( foure+"  that true");
+
+     alert( userName + "YOUR answer is yes");
+ } else{
+   
+    console.log(' oh thas wrong');
+   
+      alert( userName + "YOUR answer is false");}
+
+
+      /////////////////////////////
+      let five=prompt('is am live in acity ?');
+
+      five=five.toLowerCase();
+
+if (five=="yes" || five=="y" ) {
+
+  console.log( five+"  that true");
+
+     alert( userName + "YOUR answer is yes");
+ } else{
+   
+    console.log(' oh thas wrong');
+   
+      alert( userName + "YOUR answer is false");}
+
+      ///////
+      alert('thanks for plaing ' + userName);
